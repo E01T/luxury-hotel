b1571626133249d4bfd3683c3f0c190a1e837779
E01T <etsouderos@gmail.com>
2021-06-10 15:48:22 +0300

    Consuming data from contentful

--- src/context.js
+++ src/context.js
@@ -49,22 +49,22 @@ export default class RoomProvider extends Component {
   };
 
   componentDidMount() {
-    // this.getData();
-    let rooms = this.formatData(items);
-    let featuredRooms = rooms.filter(room => room.featured === true);
-    //
-    let maxPrice = Math.max(...rooms.map(item => item.price));
-    let maxSize = Math.max(...rooms.map(item => item.size));
-    this.setState({
-      rooms,
-      featuredRooms,
-      sortedRooms: rooms,
-      loading: false,
-      //
-      price: maxPrice,
-      maxPrice,
-      maxSize,
-    });
+    this.getData();
+    // let rooms = this.formatData(items);
+    // let featuredRooms = rooms.filter(room => room.featured === true);
+    // //
+    // let maxPrice = Math.max(...rooms.map(item => item.price));
+    // let maxSize = Math.max(...rooms.map(item => item.size));
+    // this.setState({
+    //   rooms,
+    //   featuredRooms,
+    //   sortedRooms: rooms,
+    //   loading: false,
+    //   //
+    //   price: maxPrice,
+    //   maxPrice,
+    //   maxSize,
+    // });
   }
 
   formatData(items) {
